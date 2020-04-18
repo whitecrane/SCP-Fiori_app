d90d02cb0a529c6d3afbd60f436e4d4a22c20da0
whitecrane <prg974-gruppi@yahoo.it>
2020-04-18 10:36:56 +0000

    lesson2

--- first_baseUI5app/webapp/controller/View1.controller.js
+++ first_baseUI5app/webapp/controller/View1.controller.js
@@ -1,11 +1,55 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/Device",
+	"sap/ui/model/Filter",
+	"sap/ui/model/Sorter"
+], function (Controller, Device, Filter, Sorter) {
 	"use strict";
 
 	return Controller.extend("Demo_app.first_baseUI5app.controller.View1", {
 		onInit: function () {
+			// Keeps reference to any of the created sap.m.ViewSettingsDialog-s in this sample
+			this._mViewSettingsDialogs = {};
 
-		}
+		},
+
+		createViewSettingsDialog: function (sDialogFragmentName) {
+			var oDialog = this._mViewSettingsDialogs[sDialogFragmentName];
+
+			if (!oDialog) {
+				oDialog = sap.ui.xmlfragment(sDialogFragmentName, this);
+				this._mViewSettingsDialogs[sDialogFragmentName] = oDialog;
+
+				if (Device.system.desktop) {
+					oDialog.addStyleClass("sapUiSizeCompact");
+				}
+			}
+			return oDialog;
+		},
+
+		handleSortButtonPressed: function () {
+			this.createViewSettingsDialog("Demo_app.first_baseUI5app.SortDialog").open();
+		},
+
+		handleFilterButtonPressed: function () {
+			this.createViewSettingsDialog("Demo_app.first_baseUI5app.FilterDialog").open();
+		},
+
+		handleSortDialogConfirm: function (oEvent) {
+			var oTable = this.byId("idProductsTable"),
+				mParams = oEvent.getParameters(),
+				oBinding = oTable.getBinding("items"),
+				sPath,
+				bDescending,
+				aSorters = [];
+
+			sPath = mParams.sortItem.getKey();
+			bDescending = mParams.sortDescending;
+			aSorters.push(new Sorter(sPath, bDescending));
+
+			// apply the selected sort and group settings
+			oBinding.sort(aSorters);
+		},
+		
 	});
 });
